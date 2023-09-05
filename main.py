1cb6f67c2845c3c0ff93e06c065d5bbd9e1273eb
sirikhalsa <sirikhalsanm@gmail.com>
2023-09-05 18:54:35 -0300

    added outline

---
+++ main.py
@@ -0,0 +1,4 @@
+# roll flow will use an A* graph search algorithm to determine the most efficient route between grappling positions
+# the vertices in the graph will represent positions
+# the edges will represnt valid transition between positions and their weight will indicate the relative transition difficulty
+# we will use a Euclidean heuristic for the A* algorithm so we will graph in (x,y) with diagonal movements

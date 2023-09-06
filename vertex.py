867a0342b82cb6862f0259a7a3770253e28cec19
sirikhalsa <sirikhalsanm@gmail.com>
2023-09-06 16:27:16 -0300

    added position attribute to vertex class

--- vertex.py
+++ vertex.py
@@ -1,8 +1,9 @@
 # vertex class which allows us to create graph vertices including functionality on adding edges to a vertex and getting stored
 # edges from a vertex
 class Vertex:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, name, x, y):
+        self.name = name
+        self.position = (x, y)
         self.edges = {}
 
     def add_edge(self, vertex, weight = 0):
@@ -12,4 +13,4 @@ class Vertex:
         return list(self.edges.keys())
 
     def get_weight(self, edge):
-    return self.edges[edge]
+        return self.edges[edge]
